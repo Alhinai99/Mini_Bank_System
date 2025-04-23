cb3c3b5f10c93e92a05bd1701953987bf4b4440b
alwaleed alhinai <alwaleed.h5505@gmail.com>
2025-04-23 10:00:17 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,235 @@
+﻿namespace Mini_Bank_System
+{
+    internal class Program
+    {
+        // ======Constants====
+        const double MinimumBalance = 100.0;
+        const string AccountsFilePath = "accounts.txt";
+        const string ReviewsFilePath = "reviews.txt";
+
+        //========== Global lists (parallel)==========
+        static List<int> accountNumbers = new List<int>();
+        static List<string> accountNames = new List<string>();
+        static List<double> balances = new List<double>();
+   
+        static Queue<string> createAccount = new Queue<string>(); // format: "Name|NationalID"
+        static Stack<string> reviewsStack = new Stack<string>();
+
+        // ======Account number generator=====
+        static int lastAccountNumber;
+
+
+        static void Main(string[] args)
+        {
+            WelcomeMessage();
+            systemStart();
+        }
+        // =========== Main Menu ========================
+        static void WelcomeMessage() 
+        {
+            Console.WriteLine("=========================================");
+            Console.WriteLine("Welcome to the Mini Bank System");
+            Console.WriteLine("=========================================");
+            Console.WriteLine("        ▄████▄        ");
+            Console.WriteLine("      ▄▀      ▀▄      ");
+            Console.WriteLine("     █  BANK   █     ");
+            Console.WriteLine("     █▄▄▄▄▄▄▄▄▄█     ");
+            Console.WriteLine("     █   █  █   █    ");
+            Console.WriteLine("     █   █  █   █    ");
+            Console.WriteLine("     █   █  █   █    ");
+            Console.WriteLine("     █▄▄▄█▄▄█▄▄▄█    ");
+            Console.WriteLine("    ▄████████████▄   ");
+            Console.WriteLine("   ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀   ");
+
+        }
+        static void systemStart()
+        {
+            bool ExitSystem = true;
+            
+            while (ExitSystem != false)
+            {
+
+
+                Console.WriteLine("Please select role: ");
+                Console.WriteLine("1. Admin ");
+                Console.WriteLine("2. User ");
+                Console.WriteLine("3. Exit ");
+                string role = Console.ReadLine();
+
+                switch (role)
+                {
+                    case "1":
+                        Admin();
+                        break;
+                    case "2":
+                        User();
+                        break;
+                    case "3":
+                        ExitSystem = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option, please try again.");
+                        break;
+                }
+
+
+            }
+        }
+
+        // =========== Admin Menu ========================
+        static void Admin()
+        {
+            bool AdminMenu = false;
+            Console.Clear();
+            while (AdminMenu != true)
+            {
+                Console.WriteLine("===========================");
+                Console.WriteLine("Welcome Admin");
+
+
+                Console.WriteLine("===============================");
+                Console.WriteLine("| Option      | Description    |");
+                Console.WriteLine("=============================");
+                Console.WriteLine("|   [1]     | View Requests    |");
+                Console.WriteLine("|   [2]     | View Accounts    |");
+                Console.WriteLine("|   [3]     | View Reviews     |");
+                Console.WriteLine("|   [4]     | process Requests |");
+                Console.WriteLine("|   [5]     | Exit             |");
+                Console.WriteLine("============================");
+                string option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        ViweRequest();
+                        break;
+                    case "2":
+                        ViewAccounts();
+                        break;
+                    case "3":
+                        ViewReviews();
+                        break;
+                    case "4":
+                        ProcessRequest();
+                        break;
+                    case "5":
+                        AdminMenu = true;
+                        Console.Clear();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid option, please try again.");
+                        break;
+                }
+            }
+        }
+
+        // =========== User Menu ========================
+        static void User()
+        {
+            bool UserMenu = false;
+            Console.Clear();
+            while (UserMenu != true)
+            {
+                Console.WriteLine("\n=============================");
+                Console.WriteLine("| Option    | Description |");
+                Console.WriteLine("===========================");
+                Console.WriteLine("|   [1]     | Create Account |");
+                Console.WriteLine("|   [2]     | Deposit        |");
+                Console.WriteLine("|   [3]     | Check Balance  |");
+                Console.WriteLine("|   [4]     | Submit Review  |");
+                Console.WriteLine("|   [5]     | Exit           |");
+                Console.WriteLine("============================");
+                string option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        CreateAccount();
+                        break;
+                    case "2":
+                        Deposit();
+                        break;
+                    case "3":
+                        checkBalance();
+                        break;
+                    case "4":
+                        SubmitReview();
+                        break;
+                    case "5":
+                        UserMenu = true;
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option, please try again.");
+                        break;
+                }
+            }
+        }
+
+
+        // =========== Admin Functions ========================
+
+        static void ViweRequest()
+        {
+            Console.WriteLine("View Requests");
+            
+        }
+        static void ViewAccounts()
+        {
+            Console.WriteLine("View Accounts");
+
+        }
+        static void ViewReviews()
+        {
+            Console.WriteLine("View Reviews");
+        }
+        static void ProcessRequest()
+        {
+            Console.WriteLine("Process Requests");
+        }
+
+
+        // =========== User Functions ========================
+
+        static void CreateAccount()
+        {
+            
+            Console.WriteLine("Create Account");
+            Console.WriteLine("============");
+            Console.WriteLine("Enter Your Name: ");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter Your National ID : ");
+            string ID = Console.ReadLine();
+
+            string Request = Name + "|" + ID;
+
+            createAccount.Enqueue(Request);
+
+
+        }
+        static void Deposit()
+        {
+            Console.WriteLine("Deposit");
+            Console.WriteLine("==============");
+            Console.WriteLine("Please Enter account number");
+            string 
+            Console.WriteLine("Please Enter the amount you Want to deposit : ");
+            double amount = double.Parse(Console.ReadLine());
+            
+            balances = amount + balances;
+
+        }
+        static void checkBalance()
+        {
+            Console.WriteLine("Check Balance");
+        }
+        static void SubmitReview()
+        {
+            Console.WriteLine("Submit Review");
+        }
+
+
+        
+
+
+    }
+}
